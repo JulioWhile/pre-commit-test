ec4bbd7ce8f008fcaac4355cae32a8fc6ce27723
Julio Contreras <julio.contreras0429@gmail.com>
2025-06-02 19:22:39 -0700

    Reverting test

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-
-def foo( a,  b ):
-    c =  1
-    d=2
-    unused_var = 123
-    print('The result is: ',a+b) # E225: missing whitespace around operator
-
-def bar():
-    _temp = 42  # allowed, underscore-prefixed
-    not_used = 5 # F841: assigned but never used
-    print(  "something" ) # bad spacing, single->double quote
-
-foo( 1 ,2)
+print("Hello, World!")
